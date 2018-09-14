25673049fa26d0ef4cdaf448e0a368bd153515de
Todd Chaffee <tc@bluecrow.biz>
2018-09-14 16:10:18 -0300

    feat: added heap-analytics and fullstory analytics

---
+++ src/head/analytics.js
@@ -0,0 +1,6 @@
+import fullstoryAnalytics from './fullstoryAnalytics';
+import heapAnalytics from './heapAnalytics';
+
+const analytics = [ fullstoryAnalytics, heapAnalytics ];
+
+export default analytics;
